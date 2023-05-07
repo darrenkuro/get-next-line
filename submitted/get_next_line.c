0972a23cf5aa2f0f313ba1481f30330ccb14ef70
dlu <dlu@student.42berlin.de>
2023-05-07 17:45:08 +0200

    get_next_line basic logic

--- submitted/get_next_line.c
+++ submitted/get_next_line.c
@@ -6,10 +6,24 @@
 /*   By: dlu <dlu@student.42berlin.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 03:34:01 by dlu               #+#    #+#             */
-/*   Updated: 2023/05/07 03:34:25 by dlu              ###   ########.fr       */
+/*   Updated: 2023/05/07 17:44:40 by dlu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
 char	*get_next_line(int fd)
 {
+	static char	*prev;
+	ssize_t		read_bytes;
+	char		buffer[BUFFER_SIZE];
+
+	if (fd < 0)
+		return (NULL);
+	read_bytes = read(fd, buffer, BUFFER_SIZE);
+	append_buffer(prev, buffer, read_bytes);
+	while (get_nl(buffer, read_bytes) < 0)
+	{
+		append_buffer(prev, buffer, read_bytes);
+	}
 }
--- submitted/get_next_line.h
+++ submitted/get_next_line.h
@@ -6,6 +6,21 @@
 /*   By: dlu <dlu@student.42berlin.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 03:34:55 by dlu               #+#    #+#             */
-/*   Updated: 2023/05/07 03:34:59 by dlu              ###   ########.fr       */
+/*   Updated: 2023/05/07 17:29:32 by dlu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/types.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+char	*get_next_line(int fd);
+
+#endif
