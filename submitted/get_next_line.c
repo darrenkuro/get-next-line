ac0675032c0d8b4f68432379cb9f1d96cfe0564d
dlu <dlu@student.42berlin.de>
2023-05-10 12:16:35 +0200

    change to store buffer on the heap

--- submitted/get_next_line.c
+++ submitted/get_next_line.c
@@ -6,15 +6,16 @@
 /*   By: dlu <dlu@student.42berlin.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 03:34:01 by dlu               #+#    #+#             */
-/*   Updated: 2023/05/10 01:58:22 by dlu              ###   ########.fr       */
+/*   Updated: 2023/05/10 12:16:21 by dlu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-/* Free the old prev and update with the replacement ptr and return it. */
-static char	*update_prev(char **prev, char *replace)
+/* Free the buffer and update with prev with replacement. */
+static char	*update_prev(char **prev, char *replace, char *buffer)
 {
+	free(buffer);
 	free(*prev);
 	*prev = replace;
 	return (*prev);
@@ -23,28 +24,28 @@ static char	*update_prev(char **prev, char *replace)
 /* Return the next line, and update the prev to next byte (both copy and free).
  * Prev str has to include a '\n' at index, unless it's at the end.
  * Return NULL if any malloc fails, with all allocated resources freed. */
-static char	*process_prev(char **prev, int index, int end)
+static char	*process_prev(char **prev, int index, int end, char *buffer)
 {
 	char	*line;
 	char	*tmp;
 
 	if (end && !ft_strlen(*prev))
-		return (update_prev(prev, NULL));
+		return (update_prev(prev, NULL, buffer));
 	if ((*prev)[index] == '\n')
 	{
 		line = ft_substr(*prev, 0, index + 1);
 		tmp = ft_substr(*prev, index + 1, ft_strlen(*prev) - index - 1);
-		update_prev(prev, tmp);
+		update_prev(prev, tmp, buffer);
 		if (!tmp)
-			return (free(line), NULL);
+			return (free(line), free(buffer), NULL);
 	}
 	else
 	{
 		line = ft_substr(*prev, 0, index);
-		update_prev(prev, NULL);
+		update_prev(prev, NULL, buffer);
 	}
 	if (!line)
-		return (update_prev(prev, NULL));
+		return (update_prev(prev, NULL, buffer));
 	return (line);
 }
 
@@ -52,17 +53,20 @@ static char	*process_prev(char **prev, int index, int end)
 static char	*process_next_line(int fd, char **prev)
 {
 	ssize_t	read_bytes;
-	char	buffer[BUFFER_SIZE];
+	char	*buffer;
 
+	buffer = (char *) malloc(BUFFER_SIZE * sizeof(char));
+	if (!buffer)
+		return (NULL);
 	while (nl_index(*prev, 0) < 0)
 	{
 		read_bytes = read(fd, buffer, BUFFER_SIZE);
 		if (!append_buffer(prev, buffer, read_bytes))
-			return (update_prev(prev, NULL));
+			return (update_prev(prev, NULL, buffer));
 		if (read_bytes < BUFFER_SIZE)
-			return (process_prev(prev, nl_index(*prev, 1), 1));
+			return (process_prev(prev, nl_index(*prev, 1), 1, buffer));
 	}
-	return (process_prev(prev, nl_index(*prev, 0), 0));
+	return (process_prev(prev, nl_index(*prev, 0), 0, buffer));
 }
 
 /* Get the next line of a file descriptor. */
